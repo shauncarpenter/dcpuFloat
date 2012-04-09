0d2231aa9902b64fa4d9053032d5fab5dc78968c
Shaun Carpenter <shaun.carpenter88@gmail.com>
2012-04-09 15:23:06 +0800

    Changed the implementation to the IEE 754 standard for half precision; gives an overall more pleasing form

--- floatingpointlib.asm
+++ floatingpointlib.asm
@@ -1,56 +1,53 @@
-;Floating point operations
-;Make a floating point number
+;An alternative floating point implementation
+;This one follows the IEEE 754 standard by having the format:
+; Sign|Exponent|Significand
+;    1          5              10
+
 SET PC, test
 
-:makefloat	
-;(signed integer A)
+:makefloat
+	SET PUSH, I
+	SET PUSH, J
+	SET PUSH, Z
 	
 	;body
+	;sign bit first
 	SHL A, 1
-	SET Z, O ;sign bit
-	IFN Z, 1
-		set PC, continue
+	SET Z, O
 	SHR A, 1
-	JSR negate
-	SHL A, 1
+	ADD A, 0x8000 ;to replace the sign bit lost in the shift
+	IFE Z, 1
+		JSR negate
 	
-	:continue
-	SHL Z, 15 ;put the bit in the first position
-	
-	
-	SET I, 1;
+	SET I, 0
 	:loop
-	SHL A, 1
-	ADD I, 1
-	IFN 1, O;
-		SET PC, loop
-	SHR A, I
+		ADD I, 1
+		SHL A, 1
+		IFN O, 1
+			SET PC, loop
 	
-	;Now check for a too-large integer
-	IFG I, 5
-		SET PC, smallenough
-	SET B, 6
-	SUB B, I
-	SHR A, B
-	SHL A, B
-	
-	:smallenough
-	SHL A, 5
-	ADD I, 15
-	ADD A, 15
+	;final assembly of the bits
+	SET J, 31
+	SUB J, I
+	SHL J, 10
+	SHL Z, 15
+	SHR A, 6
+	ADD A, J
+	ADD A, Z
 	
 	;end
-
+	SET Z, POP
+	SET J, POP
+	SET I, POP
 	SET PC, POP
 
-:negate 
+:negate
 ;negate A in two's complement
 	XOR A, 0xffff
-	ADD A, 1
+	ADD A, 0x1
 	SET PC, POP
 	
-	
-;Test code
+;test code
 :test
-	SET A, 0x0d95
-	JSR makefloat
+SET A, 0xFFD7
+JSR makefloat
